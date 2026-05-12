5305819439dcc8d0553089b1d208232a34ea8603
preston-k-carney-usps <preston.k.carney@usps.gov>
2026-05-12 01:40:06 -0400

    Remove js/sp-config.js

--- js/sp-config.js
+++
@@ -1,9 +0,0 @@
-/**
- * sp-config.js — SharePoint connection settings.
- * Update these values to match your SharePoint site & list.
- */
-var SP_CONFIG = {
-  siteUrl:   'https://usps365.sharepoint.com/sites/Function4Reviews73',
-  listName:  'Observations',
-  adminPass: 'F4Admin2026'   // Change this to your desired admin password
-};
